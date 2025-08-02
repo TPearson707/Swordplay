368ec42e25d579d6d21bc56e2f0c3e30351231cb
Thomas Pearson <pearson.e.thomas@gmail.com>
2025-08-02 19:33:18 -0400

    Created basic weapons using strategy pattern

---
+++ src/Claymore.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Claymore implements Weapon {
+    private int damage;
+
+    public Claymore() {
+        this.damage = 10; // Default damage value
+    }
+
+    public Claymore(int damage) {
+        if (damage <= 0) {
+            throw new IllegalArgumentException("Damage must be positive");
+        }
+        this.damage = damage;
+    }
+
+    @Override
+    public int attack() {
+        return damage;
+    }
+    
+}
---
+++ src/Dagger.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Dagger implements Weapon {
+    private int damage;
+
+    public Dagger() {
+        this.damage = 5; // Default damage value for Dagger
+    }
+
+    public Dagger(int damage) {
+        if (damage <= 0) {
+            throw new IllegalArgumentException("Damage must be positive");
+        }
+        this.damage = damage;
+    }
+
+    @Override
+    public int attack() {
+        return damage;
+    }
+}
