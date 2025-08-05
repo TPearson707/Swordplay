1314427667cc8465f26f33a351f35634348db1b1
Thomas Pearson <pearson.e.thomas@gmail.com>
2025-08-05 17:39:37 -0400

    added basic logic for staff weapon

---
+++ src/Staff.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Staff implements Weapon {
+    private int damage;
+
+    public Staff() {
+        this.damage = 8; // Default damage value for Staff
+    }
+
+    public Staff(int damage) {
+        if (damage <= 0) {
+            throw new IllegalArgumentException("Damage must be positive");
+        }
+        this.damage = damage;
+    }
+
+    @Override
+    public int attack() {
+        return damage;
+    }
+    
+}
